2bed9ab843273eda62c33908cb3bb14b1ea94fa7
Tanishka Sharma <sharmatanishka124@gmail.com>
2026-06-16 09:27:03 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Stats from "./components/Stats";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Stats />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
